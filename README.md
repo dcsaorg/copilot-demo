ac53b52ff4e6d3a93f61a3525ee030efb8baf3bb
Frazer Joe Dourado <frazer.dourado@dcsa.org>
2023-06-08 13:06:44 +0200

    init

---
+++ README.md
@@ -0,0 +1,3 @@
+# GitHub Copilot Demo
+
+We walk through the features of GitHub Copilot and understand how we can leverage AI to boot developer productivity.
---
+++ src/test/java/org/dcsa/copilotdemo/CopilotDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package org.dcsa.copilotdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CopilotDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
